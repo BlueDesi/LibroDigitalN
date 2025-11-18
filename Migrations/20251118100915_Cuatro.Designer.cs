5077f4114fd9a4fd47bfecb2219a9a52644fbc29
Javier Salinas <profejaviersalinas@gmail.com.com>
2025-11-18 07:09:56 -0300

    campo

--- Migrations/20251118100915_Cuatro.Designer.cs
+++ Migrations/20251118100915_Cuatro.Designer.cs
@@ -3,6 +3,7 @@ using LibroDigital.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibroDigital.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251118100915_Cuatro")]
+    partial class Cuatro
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251118100915_Cuatro.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibroDigital.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cuatro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Activo",
+                table: "Profesores",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Activo",
+                table: "Estudiantes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activo",
+                table: "Profesores");
+
+            migrationBuilder.DropColumn(
+                name: "Activo",
+                table: "Estudiantes");
+        }
+    }
+}
