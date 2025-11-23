640767463f8649072257b5cd688b77a3d9b7705b
BlueDesi <profejaviersalinas@gmail.com>
2025-11-22 21:09:38 -0300

    modelo Asistencia 1

--- Models/Asistencia.cs
+++ Models/Asistencia.cs
@@ -2,5 +2,12 @@
 {
     public class Asistencia
     {
+        public int Id { get; set; }
+        public int EstudianteId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int CursoId { get; set; }
+        public string Comentario { get; set; }=string.Empty;
+        public bool Presente { get; set; }
+        public int EventoId { get; set; }
     }
 }
